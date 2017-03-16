7f698e2fb51446c154d11ccab4033db7c15bc856
Rael Kenny <RaelKenny@gmail.com>
2017-03-16 16:11:28 -0400

    inital commit

---
+++ All For One/SignUpViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SignUpViewController.swift
+//  All For One
+//
+//  Created by Rael Kenny on 3/16/17.
+//  Copyright © 2017 Rael Kenny. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+}
